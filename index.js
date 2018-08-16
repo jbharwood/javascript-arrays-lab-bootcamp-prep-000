edc715eb0df1e3d82f282b2b551525d0cf6111da
Joseph Harwood <josephblakeharwood@gmail.com>
2018-08-16 19:05:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,6 +24,6 @@ function prependKitten(name) {
   return kittens1
 }
 function removeLastKitten() {
-  var kittens1 = kittens.slice(kitens.length, 1)
+  var kittens1 = kittens.slice(kittens.length, 1)
   return kittens1
 }
