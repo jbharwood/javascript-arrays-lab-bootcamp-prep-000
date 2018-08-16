42e9d2d90f5420c861feecae97e5dab22eac9fe9
Joseph Harwood <josephblakeharwood@gmail.com>
2018-08-16 18:39:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your functions and code here
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+}
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
+function appendKitten(name) {
+  kittens = kittens.splice(kittens.length, 0, name)
+  return kittens
+}
